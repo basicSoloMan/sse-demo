5c5f1c9707f410758b3693717f2f3fe84304e3b2
marius <mariusjansevanrensburg1@gmail.com>
2025-06-09 18:20:36 +0200

    feat: sse demo add minimal api to showcase sse at a very low level

--- sss-demo/sss-demo/DataGenerator.cs
+++ sss-demo/sss-demo/DataGenerator.cs
@@ -1,6 +1,21 @@
 ﻿namespace sss_demo;
 
-public class DataGenerator
+public static class DataGenerator
 {
-    
+    public static MockDataModel CreateMockData()
+    {
+        var random = new Random();
+
+        return new MockDataModel
+        {
+            Identifier = Guid.NewGuid(),
+            Random = random.Next(10)
+        };
+    }
+}
+
+public class MockDataModel
+{
+    public Guid Identifier { get; set; }
+    public int Random { get; set; }
 }
---
+++ sss-demo/sss-demo/Program.cs
@@ -0,0 +1,39 @@
+using System.Net.ServerSentEvents;
+using System.Runtime.CompilerServices;
+using sss_demo;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+app.MapGet("/sse-demo", (CancellationToken cancellationToken) =>
+{
+    return TypedResults.ServerSentEvents(GetData(cancellationToken));
+    
+    async IAsyncEnumerable<SseItem<MockDataModel>> GetData([EnumeratorCancellation] CancellationToken ct)
+    {
+        while (!ct.IsCancellationRequested)
+        {
+            await Task.Delay(TimeSpan.FromSeconds(2), ct);
+            yield return new SseItem<MockDataModel>(DataGenerator.CreateMockData(), "mock-data")
+            {
+                ReconnectionInterval = TimeSpan.FromMinutes(1),
+            };
+        }
+    } 
+});
+
+app.Run();
